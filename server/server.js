aa68d8f37156ddb0f5cb2c4c7b11171b226b2c07
xANARKYx <shikhars3145@gmail.com>
2020-08-26 13:50:04 +0530

    DB connected

--- server/server.js
+++ server/server.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.log('error connecting db', err));
 
 const app = express();
 
