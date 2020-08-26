15274859ed3991846faafd7a6fb25c745cfe57c6
xANARKYx <shikhars3145@gmail.com>
2020-08-26 14:18:28 +0530

    basic applicant model created

---
+++ server/models/applicant.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const applicantSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'An Applicant must have a name'],
+    },
+    notes: {
+      type: String,
+    },
+    technologies: [String],
+  },
+  { timestamps: true }
+);
+
+const Applicant = mongoose.model('Applicant', applicantSchema);
