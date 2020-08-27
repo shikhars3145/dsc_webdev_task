00e689f56819398e1ce73b12397f2464dc452c67
xANARKYx <shikhars3145@gmail.com>
2020-08-28 03:47:44 +0530

    updated post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -3,17 +3,44 @@ const Schema = mongoose.Schema;
 
 const postSchema = new Schema(
   {
-    name: {
+    team: {
       type: String,
-      required: [true, 'A post must have a name'],
+      required: [true, 'A post must have a team'],
+    },
+    position: {
+      type: String,
+      required: [true, 'A post must have a position'],
     },
     description: {
       type: String,
+      required: [true, 'A post must have a Description'],
     },
     technologies: [String],
+    openPositions: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: String,
+      default: 'default',
+    },
+    noOfApplicants: {
+      type: Number,
+      default: 0,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
+
+postSchema.virtual('applications', {
+  ref: 'applications',
+  foreignField: 'post',
+  localField: '_id',
+});
 
 const Post = mongoose.model('posts', postSchema);
 
